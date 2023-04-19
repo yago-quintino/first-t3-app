0865c351374ce347deaceba179da6d859869701d
yago-quintino <yagoquintino21@gmail.com>
2023-04-19 17:45:25 -0300

    Update profile page, routes and fix document Head and index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,16 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Emojis T3 App</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,10 +1,10 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "../trpc";
-import type { User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
 import z from "zod";
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -18,14 +18,6 @@ const ratelimit = new Ratelimit({
    */
   // prefix: "@upstash/ratelimit",
 });
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
